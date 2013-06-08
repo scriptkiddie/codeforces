f31d7185f517aff483f827936787185d0dad8d83
George <somu@so.mu>
2013-06-09 00:13:43 +0400

    Bunch o’ options

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,31 +1,35 @@
 # encoding: utf-8
 module Codeforces
-  # require 'numbers_and_words'
   require 'ruby-progressbar'
   require 'nokogiri'
   require 'open-uri'
 
   class Parser; class << self
-    def convert html
-      html.gsub(/<.+?>(.+)<\/.+?>/) { $1.gsub(/<br\/?>/, "\n") }
+    def convert node
+      node.to_html.gsub(/<.+?>(.+)<\/.+?>/) { $1.gsub(/<br\/?>/, "\n") }
     end
     def parse id
       problems = {}
-      bar = ProgressBar.create(title: 'Задания', total: 26)
+      bar = ProgressBar.create title: 'Prolems', total: 26 # latin alphabet
       ('A'..'Z').each do |letter|
         problem = Nokogiri::HTML open "http://codeforces.ru/contest/#{id}/problem/#{letter}?locale=en"
         input, output = [], []
 
         problem.css('.sample-test').each do |test|
-          input << convert(test.css('.input pre').first.to_html)
-          output << convert(test.css('.output pre').first.to_html)
+          input << convert(test.css('.input pre').first)
+          output << convert(test.css('.output pre').first)
         end
 
         break if input.empty? or output.empty?
-        problems.merge! problem.css('.header .title').text.downcase.gsub(' ', '-').gsub(/[^\w-]/, '')[2..-1] => [input, output]
+        problems.merge! letter => {
+          :name => problem.css('.header .title').text,
+          :input => input,
+          :output => output
+        }
+
         bar.increment
       end
-      p problems
+      problems
     end
   end end
 end
